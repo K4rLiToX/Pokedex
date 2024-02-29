5250d1e42f30e59a5e22ef0649a2d056734cf5d4
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-02-29 09:15:31 +0100

    chore: bump dependencies versions

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("io.ktor:ktor-client-android:2.3.8")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.8")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
-    implementation("com.google.dagger:hilt-android:2.50")
+    implementation("com.google.dagger:hilt-android:2.51")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    ksp("com.google.dagger:hilt-android-compiler:2.50")
+    ksp("com.google.dagger:hilt-android-compiler:2.51")
 }
