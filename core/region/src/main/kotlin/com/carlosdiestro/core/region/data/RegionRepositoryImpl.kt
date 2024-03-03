a61b6535992f025119f517bfa44c7c7250420566
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-03-03 11:13:17 +0100

    refactor: cache data

--- core/region/src/main/kotlin/com/carlosdiestro/core/region/data/RegionRepositoryImpl.kt
+++ core/region/src/main/kotlin/com/carlosdiestro/core/region/data/RegionRepositoryImpl.kt
@@ -2,11 +2,29 @@ package com.carlosdiestro.core.region.data
 
 import com.carlosdiestro.core.region.domain.Region
 import com.carlosdiestro.core.region.domain.RegionRepository
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class RegionRepositoryImpl @Inject constructor(
     private val remote: RegionRemoteDatasource,
     private val local: RegionLocalDatasource
 ): RegionRepository {
-    override suspend fun getPokemonRegions(): Result<List<Region>> = remote.getPokemonRegions()
+    override suspend fun getPokemonRegions(): Result<List<Region>> {
+        val cachedRegions = local.getAll().first()
+        val isCacheEmpty = cachedRegions.isEmpty()
+
+        return if (isCacheEmpty) {
+            remote.getPokemonRegions().fold(
+                onSuccess = { regions ->
+                    local.upsert(regions)
+                    Result.success(regions)
+                },
+                onFailure = { e ->
+                    Result.failure(e)
+                }
+            )
+        } else {
+            Result.success(cachedRegions)
+        }
+    }
 }
