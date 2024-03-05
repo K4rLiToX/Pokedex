1c88dc39fee6d417e4bb35d37893588f8a34cf53
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-03-05 18:42:56 +0100

    refactor: remove foreign key

--- database/src/main/kotlin/com/carlosdiestro/database/pokedex/PokedexEntity.kt
+++ database/src/main/kotlin/com/carlosdiestro/database/pokedex/PokedexEntity.kt
@@ -1,21 +1,9 @@
 package com.carlosdiestro.database.pokedex
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
-import com.carlosdiestro.database.region.RegionEntity
 
-@Entity(
-    tableName = "pokedexes_table",
-    foreignKeys = [
-        ForeignKey(
-            entity = RegionEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["regionId"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ]
-)
+@Entity(tableName = "pokedexes_table")
 data class PokedexEntity(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
