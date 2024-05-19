b9e945e994b93616a40a3b09adb46b5afa4f6f41
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-05-19 18:47:26 +0200

    feat: enable contrasting colors schemes

--- design_system/src/main/kotlin/com/carlosdiestro/design_system/theme/Theme.kt
+++ design_system/src/main/kotlin/com/carlosdiestro/design_system/theme/Theme.kt
@@ -7,12 +7,16 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.carlosdiestro.design_system.theme.SchemeColorContrast.High
+import com.carlosdiestro.design_system.theme.SchemeColorContrast.Medium
+import com.carlosdiestro.design_system.theme.SchemeColorContrast.Standard
 
 @Composable
 fun PokedexTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    schemeColorContrast: SchemeColorContrast = Standard,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
@@ -21,8 +25,21 @@ fun PokedexTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme                                                      -> darkScheme
-        else                                                           -> lightScheme
+        darkTheme                                                      -> {
+            when (schemeColorContrast) {
+                Standard -> darkScheme
+                Medium   -> mediumContrastDarkColorScheme
+                High     -> highContrastDarkColorScheme
+            }
+        }
+
+        else                                                           -> {
+            when (schemeColorContrast) {
+                Standard -> lightScheme
+                Medium   -> mediumContrastLightColorScheme
+                High     -> highContrastLightColorScheme
+            }
+        }
     }
 
     MaterialTheme(
@@ -30,4 +47,10 @@ fun PokedexTheme(
         typography = PokedexTypography,
         content = content
     )
+}
+
+enum class SchemeColorContrast {
+    Standard,
+    Medium,
+    High
 }
