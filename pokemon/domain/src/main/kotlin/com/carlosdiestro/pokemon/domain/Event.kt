a406984ea30839e2f9075836d437ed99a406464a
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-05-26 12:28:40 +0200

    refactor: grammar corrections

--- pokemon/domain/src/main/kotlin/com/carlosdiestro/pokemon/domain/Event.kt
+++ pokemon/domain/src/main/kotlin/com/carlosdiestro/pokemon/domain/Event.kt
@@ -4,5 +4,5 @@ sealed interface Event<out T> {
 
     data class Success<T>(val data: T) : Event<T>
     data object DataNotAvailable : Event<Nothing>
-    data object DataNotModifier : Event<Nothing>
+    data object DataNotModified : Event<Nothing>
 }
