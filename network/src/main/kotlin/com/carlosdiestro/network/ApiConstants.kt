fef277a9d47b4cc690d18f14afea0779044b5875
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-05-27 19:06:35 +0200

    feat: add base pokemon image url

--- network/src/main/kotlin/com/carlosdiestro/network/ApiConstants.kt
+++ network/src/main/kotlin/com/carlosdiestro/network/ApiConstants.kt
@@ -8,6 +8,7 @@ internal object ApiConstants {
 internal object ApiRoutes {
 
     private const val BASE_URL = "https://pokeapi.co/api/v2"
+    const val BASE_POKEMON_SPRITE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
     const val NATIONAL_DEX_ENTRIES = "$BASE_URL/pokedex/1"
     const val POKEMON_SPECIES = "$BASE_URL/pokemon-species"
     const val POKEMON_DETAILS = "$BASE_URL/pokemon"
