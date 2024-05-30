f77fb7c2290031eaf7683db04ff2d10c9c739635
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-05-30 12:35:20 +0200

    refactor: remove object wrapper

--- network/src/test/kotlin/com/carlosdiestro/network/TestUtils.kt
+++ network/src/test/kotlin/com/carlosdiestro/network/TestUtils.kt
@@ -18,87 +18,86 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.Assert
 
-internal object TestUtils {
 
-    val pokemonId = 1
-    val pokemonEntries = listOf(
-        PokemonEntryDto(
-            order = 1,
-            pokemonSpecies = SimplePokemonSpeciesDto(
-                name = "",
-                url = "/$pokemonId"
-            )
+internal const val pokemonId = 1
+
+internal val pokemonEntries = listOf(
+    PokemonEntryDto(
+        order = 1,
+        pokemonSpecies = SimplePokemonSpeciesDto(
+            name = "",
+            url = "/$pokemonId"
         )
     )
-    val pokemon = PokemonDto(
-        id = pokemonId,
-        name = "",
-        order = 1,
-        types = listOf(
-            TypeDto(
-                slot = 0,
-                type = SimpleTypeDto(
-                    name = "",
-                    url = ""
-                )
-            )
-        ),
-        height = 0,
-        weight = 0,
-        eggGroups = listOf(
-            EggGroupDto(
+)
+
+internal val pokemon = PokemonDto(
+    id = pokemonId,
+    name = "",
+    order = 1,
+    types = listOf(
+        TypeDto(
+            slot = 0,
+            type = SimpleTypeDto(
                 name = "",
                 url = ""
             )
-        ),
-        abilities = listOf(
-            AbilityDto(
-                ability = SimpleAbilityDto(
-                    name = "",
-                    url = ""
-                )
+        )
+    ),
+    height = 0,
+    weight = 0,
+    eggGroups = listOf(
+        EggGroupDto(
+            name = "",
+            url = ""
+        )
+    ),
+    abilities = listOf(
+        AbilityDto(
+            ability = SimpleAbilityDto(
+                name = "",
+                url = ""
             )
-        ),
-        stats = listOf(
-            StatDto(
-                statValue = 0,
-                stat = SimpleStatDto(
-                    name = "",
-                    url = ""
-                )
+        )
+    ),
+    stats = listOf(
+        StatDto(
+            statValue = 0,
+            stat = SimpleStatDto(
+                name = "",
+                url = ""
             )
         )
     )
+)
 
-
-    fun <T> createPokeApiWithResult(
-        result: ApiResult<T>,
-        apiFunction: suspend PokeApi.() -> ApiResult<T>,
-    ): PokeApi {
-        return mockk<PokeApi> {
-            coEvery { apiFunction() } returns result
-        }
+internal fun <T> createPokeApiWithResult(
+    result: ApiResult<T>,
+    apiFunction: suspend PokeApi.() -> ApiResult<T>,
+): PokeApi {
+    return mockk<PokeApi> {
+        coEvery { apiFunction() } returns result
     }
+}
 
-    fun <T> Event<T>.assertSuccess() {
-        Assert.assertTrue(this is Success)
-    }
+internal fun <T> Event<T>.assertSuccess() {
+    Assert.assertTrue(this is Success)
+}
 
-    fun <T> Event<T>.assertSuccessAndEmpty() {
-        this.assertSuccess()
-        Assert.assertTrue(((this as Success).data as List<*>).isEmpty())
-    }
+internal fun <T> Event<T>.assertSuccessAndEmpty() {
+    this.assertSuccess()
+    Assert.assertTrue(((this as Success).data as List<*>).isEmpty())
+}
 
-    fun <T> Event<T>.assertSuccessAndNotEmpty() {
-        this.assertSuccess()
-        Assert.assertTrue(((this as Success).data as List<*>).isNotEmpty())
-    }
+internal fun <T> Event<T>.assertSuccessAndNotEmpty() {
+    this.assertSuccess()
+    Assert.assertTrue(((this as Success).data as List<*>).isNotEmpty())
+}
 
-    fun <T> Event<T>.assertDataNotModified() {
-        Assert.assertTrue(this is DataNotModified)
-    }
+internal fun <T> Event<T>.assertDataNotModified() {
+    Assert.assertTrue(this is DataNotModified)
+}
 
-    fun <T> Event<T>.assertDataNotAvailable() {
-        Assert.assertTrue(this is DataNotAvailable)
-    }
+internal fun <T> Event<T>.assertDataNotAvailable() {
+    Assert.assertTrue(this is DataNotAvailable)
 }
