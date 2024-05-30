7ad781226ae34575d8f85c50ceb783c6567ed530
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-05-30 14:24:03 +0200

    build: add di dependencies

--- pokemon/data/build.gradle.kts
+++ pokemon/data/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     alias(libs.plugins.pokedex.jvm.library)
+    alias(libs.plugins.ksp)
 }
 
 dependencies {
     api(projects.pokemon.domain)
+
+    implementation(libs.hilt.core)
+    implementation(libs.hilt.compiler)
 
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
