35f8433b2b0438111995d0be27bd9b59a7d48e40
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-01 10:17:40 +0200

    chore: initialize di in the whole app

--- app/src/main/kotlin/com/carlosdiestro/pokedex/MainActivity.kt
+++ app/src/main/kotlin/com/carlosdiestro/pokedex/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.carlosdiestro.design_system.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
