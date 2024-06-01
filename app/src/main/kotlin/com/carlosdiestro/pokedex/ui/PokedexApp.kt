eb74254c9e7edfeb0ff0e0b28046df54a906932f
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-01 16:30:12 +0200

    feat: handle insets

--- app/src/main/kotlin/com/carlosdiestro/pokedex/ui/PokedexApp.kt
+++ app/src/main/kotlin/com/carlosdiestro/pokedex/ui/PokedexApp.kt
@@ -1,6 +1,12 @@
 package com.carlosdiestro.pokedex.ui
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -19,7 +25,15 @@ internal fun PokedexApp(
         PokedexNavHost(
             appState = appState,
             modifier = modifier
-                .padding(innerPadding)
+                .padding(
+                    top = innerPadding.calculateTopPadding()
+                )
+                .consumeWindowInsets(innerPadding)
+                .windowInsetsPadding(
+                    WindowInsets.safeDrawing.only(
+                        WindowInsetsSides.Horizontal
+                    )
+                )
         )
     }
 }
