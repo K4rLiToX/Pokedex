9f0aaa607c656dd9a93736403e02f4f5ca811076
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-01 16:28:46 +0200

    refactor: make system bars transparent

--- app/src/main/kotlin/com/carlosdiestro/pokedex/MainActivity.kt
+++ app/src/main/kotlin/com/carlosdiestro/pokedex/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.carlosdiestro.pokedex
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +18,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             val appState = rememberPokedexAppState()
 
