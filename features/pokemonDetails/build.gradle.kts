971762d36b94d8ac231910a66a3384ef740a435e
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-09 09:44:06 +0200

    build: apply convention plugins

--- features/pokemonDetails/build.gradle.kts
+++ features/pokemonDetails/build.gradle.kts
@@ -1,37 +1,31 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.pokedex.android.library)
+    alias(libs.plugins.pokedex.android.library.compose)
+    alias(libs.plugins.pokedex.android.hilt)
+    alias(libs.plugins.pokedex.android.feature)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.carlosdiestro.features.pokemondetails"
-    compileSdk = 34
 
     defaultConfig {
-        minSdk = 30
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    implementation(projects.pokemon.domain)
 
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.palette.ktx)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
