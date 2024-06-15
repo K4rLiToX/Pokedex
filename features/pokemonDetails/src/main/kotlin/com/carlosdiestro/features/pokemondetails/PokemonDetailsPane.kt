7650982b8918cb79c2fad4bb25048f3197b2bfc3
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-15 18:35:10 +0200

    refactor: implement correct ui

--- features/pokemonDetails/src/main/kotlin/com/carlosdiestro/features/pokemondetails/PokemonDetailsPane.kt
+++ features/pokemonDetails/src/main/kotlin/com/carlosdiestro/features/pokemondetails/PokemonDetailsPane.kt
@@ -3,6 +3,7 @@ package com.carlosdiestro.features.pokemondetails
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,19 +12,31 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,9 +44,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -52,8 +67,6 @@ import com.carlosdiestro.features.pokemondetails.state.PokemonDetailsUiState
 import com.carlosdiestro.pokemon.domain.models.PokemonAbility
 import com.carlosdiestro.pokemon.domain.models.PokemonDetails
 import com.carlosdiestro.pokemon.domain.models.PokemonEggGroup
-import com.carlosdiestro.pokemon.domain.models.PokemonStat
-import com.carlosdiestro.pokemon.domain.models.PokemonType
 
 @Composable
 internal fun PokemonDetailsPane(
@@ -64,7 +77,9 @@ internal fun PokemonDetailsPane(
 
     PokemonDetailsPane(
         onBack = onBack,
-        state = state
+        state = state,
+        modifier = Modifier
+            .fillMaxSize()
     )
 }
 
@@ -75,142 +90,162 @@ private fun PokemonDetailsPane(
     state: PokemonDetailsUiState,
     modifier: Modifier = Modifier,
 ) {
-    val defaultContentColor = MaterialTheme.colorScheme.surface
-
-    var contentColor by remember {
-        mutableStateOf(defaultContentColor)
+    val defaultBackgroundColor = MaterialTheme.colorScheme.surface
+    var backgroundColor by remember {
+        mutableStateOf(defaultBackgroundColor)
     }
+    val sheetState = rememberStandardBottomSheetState()
+    val scaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = sheetState
+    )
+    val sheetHeight = LocalConfiguration.current.screenHeightDp.dp - 40.dp
 
-    Scaffold(
+    val contentVisibility by animateFloatAsState(
+        targetValue = if (sheetState.targetValue == SheetValue.Expanded) 0F else 1F,
+        label = "Content Visibility"
+    )
+
+    val titleVisibility by animateFloatAsState(
+        targetValue = if (sheetState.targetValue == SheetValue.PartiallyExpanded) 0F else 1F,
+        label = "Title Visibility"
+    )
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
         topBar = {
-            when (state) {
-                is PokemonDetailsUiState.Success -> {
-                    TopAppBar(
-                        title = {
+            TopAppBar(
+                title = {
+                    when (state) {
+                        is PokemonDetailsUiState.Success -> {
                             Text(
-                                text = "#${state.pokemon.entry.order.value}"
+                                text = state.pokemon.entry.name.value,
+                                modifier = Modifier
+                                    .alpha(titleVisibility)
                             )
-                        },
-                        navigationIcon = {
-                            IconButton(
-                                onClick = onBack
-                            ) {
-                                Icon(
-                                    imageVector = PokedexIcons.Back,
-                                    contentDescription = stringResource(id = R.string.back_action_content_description)
-                                )
-                            }
                         }
-                    )
-                }
 
-                else                             -> Unit
+                        else                             -> Unit
+                    }
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onBack
+                    ) {
+                        Icon(
+                            imageVector = PokedexIcons.Back,
+                            contentDescription = stringResource(id = R.string.back_action_content_description)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = backgroundColor
+                )
+            )
+        },
+        sheetContent = {
+            when (state) {
+                is PokemonDetailsUiState.Loading          -> LoadingLayout(Modifier.fillMaxSize())
+                is PokemonDetailsUiState.Empty            -> EmptyLayout(Modifier.fillMaxSize())
+                is PokemonDetailsUiState.DataNotAvailable -> DataNotAvailableLayout(Modifier.fillMaxSize())
+                is PokemonDetailsUiState.Success          -> {
+                    val pokemon = state.pokemon
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .requiredHeight(sheetHeight)
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.about_title),
+                            style = MaterialTheme.typography.titleSmall,
+                            modifier = Modifier
+                                .padding(horizontal = 24.dp)
+                                .padding(top = 24.dp)
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Specs(
+                            height = pokemon.height.value,
+                            weight = pokemon.weight.value,
+                            eggGroups = pokemon.eggGroups,
+                            abilities = pokemon.abilities,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 24.dp)
+                        )
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            text = stringResource(id = R.string.stats_title),
+                            style = MaterialTheme.typography.titleSmall,
+                            modifier = Modifier
+                                .padding(horizontal = 24.dp)
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        pokemon.stats.forEach { stat ->
+                            Stat(
+                                label = stat.name,
+                                value = stat.value,
+                                modifier = Modifier
+                                    .padding(horizontal = 24.dp)
+                            )
+                        }
+                    }
+                }
             }
         },
-        contentColor = contentColor,
+        sheetPeekHeight = 500.dp,
+        sheetContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+        sheetContentColor = MaterialTheme.colorScheme.onSurface,
+        sheetTonalElevation = 0.dp,
+        sheetDragHandle = {},
+        sheetShape = RectangleShape,
         modifier = modifier
-            .fillMaxSize()
-    ) { contentPadding ->
+    ) { innerPadding ->
         when (state) {
-            is PokemonDetailsUiState.Loading -> LoadingLayout()
-            is PokemonDetailsUiState.DataNotAvailable -> DataNotAvailableLayout()
-            is PokemonDetailsUiState.Empty -> EmptyLayout()
-            is PokemonDetailsUiState.Success -> PokemonDetailsLayout(
-                pokemon = state.pokemon,
-                contentPadding = contentPadding,
-                contentColor = {
-                    contentColor = it
-                },
-                modifier = Modifier
-                    .fillMaxSize()
-            )
-        }
-    }
-}
-
-@Composable
-private fun PokemonDetailsLayout(
-    pokemon: PokemonDetails,
-    contentPadding: PaddingValues,
-    contentColor: (Color) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Box(
-        modifier = modifier
-    ) {
-        AsyncImage(
-            model = pokemon.entry.spriteUrl.value,
-            contentDescription = pokemon.entry.name.value,
-            contentScale = ContentScale.Fit,
-            onSuccess = {
-                calcDominantColor(it.result.drawable) { color ->
-                    contentColor(color)
+            is PokemonDetailsUiState.Loading          -> LoadingLayout(Modifier.fillMaxSize())
+            is PokemonDetailsUiState.Empty            -> EmptyLayout(Modifier.fillMaxSize())
+            is PokemonDetailsUiState.DataNotAvailable -> DataNotAvailableLayout(Modifier.fillMaxSize())
+            is PokemonDetailsUiState.Success          -> {
+                Column(
+                    modifier = Modifier
+                        .padding(top = innerPadding.calculateTopPadding())
+                        .background(backgroundColor)
+                        .padding(horizontal = 16.dp)
+                        .alpha(contentVisibility)
+                ) {
+                    Text(
+                        text = state.pokemon.entry.name.value,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        state.pokemon.types.forEach { type ->
+                            Type(label = type.value)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        AsyncImage(
+                            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${state.pokemon.entry.id.value}.gif",
+                            contentDescription = state.pokemon.entry.name.value,
+                            contentScale = ContentScale.Fit,
+                            onSuccess = {
+                                calcDominantColor(it.result.drawable) { color ->
+                                    backgroundColor = color
+                                }
+                            },
+                            modifier = Modifier
+                                .width(140.dp)
+                                .aspectRatio(1F)
+                        )
+                    }
                 }
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(contentPadding)
-                .aspectRatio(1F)
-        )
-        Column(
-            verticalArrangement = Arrangement.spacedBy(32.dp),
-            modifier = Modifier
-                .clip(RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp))
-                .background(MaterialTheme.colorScheme.surfaceContainerLowest)
-                .padding(
-                    start = 16.dp,
-                    top = 90.dp,
-                    end = 16.dp,
-                    bottom = 16.dp
-                )
-        ) {
-            Header(
-                name = pokemon.entry.name.value,
-                types = pokemon.types,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-            Specs(
-                height = pokemon.height.value,
-                weight = pokemon.weight.value,
-                eggGroups = pokemon.eggGroups,
-                abilities = pokemon.abilities
-            )
-            Stats(
-                stats = pokemon.stats,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-        }
-    }
-}
-
-@Composable
-private fun Header(
-    name: String,
-    types: List<PokemonType>,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        Text(
-            text = name,
-            style = MaterialTheme.typography.headlineMedium,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            types.forEach { type ->
-                Type(label = type.value)
             }
         }
     }
@@ -235,34 +270,12 @@ private fun Specs(
     FlowRow(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        maxItemsInEachRow = 2,
         modifier = modifier
     ) {
         HeightSpec(value = height.toString())
         WeightSpec(value = weight.toString())
         EggGroupSpec(values = eggGroup)
         AbilitySpec(values = ability)
-    }
-}
-
-@Composable
-private fun Stats(
-    stats: List<PokemonStat>,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = modifier
-    ) {
-        Text(
-            text = stringResource(id = R.string.stats_title)
-        )
-        stats.forEach { stat ->
-            Stat(
-                label = stat.name,
-                value = stat.value
-            )
-        }
     }
 }
 
