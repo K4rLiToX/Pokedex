0c7a07e94184fcb0ffdb7f31558cdb9b5cd56f53
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-06-23 11:11:13 +0200

    build: create list details module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(projects.network)
     implementation(projects.features.pokemonEntries)
     implementation(projects.features.pokemonDetails)
+    implementation(projects.features.pokemonEntriesListDetails)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
